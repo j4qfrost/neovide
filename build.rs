aba614a90be9742dd10acd5fdb5aebf70fcf1f72
AnhQuan Nguyen <j4qfrost@gmail.com>
2020-06-11 05:42:56 -0500

    add gcc 10 build fix for linux

--- build.rs
+++ build.rs
@@ -5,4 +5,18 @@ fn main() {
         res.set_icon("assets/nvim.ico");
         res.compile().expect("Could not attach exe icon");
     }
+
+    #[cfg(target_os = "linux")]
+    {
+        use std::env;
+        let cc = env::var("CC").unwrap();
+        if let Ok(version) = process::Command::new(cc).arg("-dumpversion").output() {
+            let local_ver = Version::from(version.stdout).unwrap();
+            let affected_ver = Version::from("10").unwrap();
+
+            if local_ver >= affected_ver {
+                env::set_var("CFLAGS", "-fcommon -fPIE");
+            }
+        }
+    }
 }
