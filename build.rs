0c9660137b55f879825efe0c2a66f47243724e0e
AnhQuan Nguyen <j4qfrost@gmail.com>
2020-06-11 06:07:35 -0500

    check if gcc-10 is installed correctly in runner

--- build.rs
+++ build.rs
@@ -6,17 +6,17 @@ fn main() {
         res.compile().expect("Could not attach exe icon");
     }
 
-    #[cfg(target_os = "linux")]
-    {
-        use std::env;
-        let cc = env::var("CC").unwrap();
-        if let Ok(version) = process::Command::new(cc).arg("-dumpversion").output() {
-            let local_ver = Version::from(version.stdout).unwrap();
-            let affected_ver = Version::from("10").unwrap();
+    // #[cfg(target_os = "linux")]
+    // {
+    //     use std::env;
+    //     let cc = env::var("CC").unwrap();
+    //     if let Ok(version) = process::Command::new(cc).arg("-dumpversion").output() {
+    //         let local_ver = Version::from(version.stdout).unwrap();
+    //         let affected_ver = Version::from("10").unwrap();
 
-            if local_ver >= affected_ver {
-                env::set_var("CFLAGS", "-fcommon -fPIE");
-            }
-        }
-    }
+    //         if local_ver >= affected_ver {
+    //             env::set_var("CFLAGS", "-fcommon -fPIE");
+    //         }
+    //     }
+    // }
 }
